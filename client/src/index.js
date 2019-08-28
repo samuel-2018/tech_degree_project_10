2a251546b612b7ede0eed8c2c8144b86adce0848
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-27 20:25:10 -0400

    added BrowserRouter, ErrorBoundary,  and Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import './index.css';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { Provider } from "./helpers/context";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ErrorBoundary>
+      <Provider>
+        <App />
+      </Provider>
+    </ErrorBoundary>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

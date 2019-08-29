703b27a11561db97af5099dfdd86e57cbf155ede
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-29 08:12:22 -0400

    Prettier changed '  to "

--- api/db/models/user.js
+++ api/db/models/user.js
@@ -1,50 +1,53 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    'User',
+    "User",
     {
       firstName: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: { msg: 'First name is required.' },
-        },
+          notEmpty: { msg: "First name is required." }
+        }
       },
       lastName: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: { msg: 'Last name is required.' },
-        },
+          notEmpty: { msg: "Last name is required." }
+        }
       },
       emailAddress: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: { msg: 'Email address is required.' },
-          isEmail: { msg: 'Valid email address is required.' },
+          notEmpty: { msg: "Email address is required." },
+          isEmail: { msg: "Valid email address is required." }
         },
-        unique: { msg: 'That email address is already associated with an account.' },
+        unique: {
+          msg: "That email address is already associated with an account."
+        }
       },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: { msg: 'Password is required.' },
-        },
-      },
+          notEmpty: { msg: "Password is required." }
+        }
+      }
     },
     {
       // Options
-    },
+    }
   );
 
-  User.associate = (models) => {
+  User.associate = models => {
     User.hasMany(models.Course, {
-      // Will save to database ok without this,
-      // but needed for retrieval of key
-      foreignKey: 'userId',
       // If client doesn't supply a userId, API will respond with
       // "SQLITE_CONSTRAINT: FOREIGN KEY constraint failed"
+
+      // Will save to database ok without this,
+      // but needed for retrieval of key
+      foreignKey: "userId"
     });
   };
 

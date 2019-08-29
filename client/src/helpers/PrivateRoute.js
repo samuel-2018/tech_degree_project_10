1e24db48f11a045485873d3761b57eda86840b53
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-29 08:34:50 -0400

    made into stateless function, default export:
    Returns stateless function instead of class, changed from named export to default export

--- client/src/helpers/PrivateRoute.js
+++ client/src/helpers/PrivateRoute.js
@@ -6,42 +6,34 @@ import { Route, Redirect } from "react-router-dom";
 
 // HOC
 function PrivateRoute(OriginalComponent) {
-  return class extends Component {
-    // constructor() {
-    //   super();
-    //   this.state = {
-
-    //   };
-
-    // }
-    render() {
-      const { location } = this.props;
-      return (
-        <Consumer>
-          {context => {
-            return (
-              // Is the user signed in?
-              context.authenticatedUser ? (
-                // Yes, render requested page.
-
-                // TO DO  Should this be  "context={context}"? YES CHANGED
-
-                <OriginalComponent context={context} {...this.props} />
-              ) : (
-                // No, redirect to signin.
-                <Redirect
-                  to={{
-                    pathname: `/signin`,
-                    state: { from: location }
-                  }}
-                />
-              )
-            );
-          }}
-        </Consumer>
-      );
-    }
+  return function(...props) {
+    const { location } = props;
+    return (
+      <Consumer>
+        {context => {
+          return (
+            <Route
+              render={props =>
+                // Is the user signed in?
+                context.authenticatedUser ? (
+                  // Yes, render requested page.
+                  <OriginalComponent context={context} {...props} />
+                ) : (
+                  // No, redirect to signin.
+                  <Redirect
+                    to={{
+                      pathname: `/signin`,
+                      state: { from: location }
+                    }}
+                  />
+                )
+              }
+            />
+          );
+        }}
+      </Consumer>
+    );
   };
 }
 
-export { PrivateRoute };
+export default PrivateRoute;

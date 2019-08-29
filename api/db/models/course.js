18bad7e4c50d188f6f7a7e83dc9a94d2b20becf6
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-29 08:12:02 -0400

    Prettier changed '  to "

--- api/db/models/course.js
+++ api/db/models/course.js
@@ -1,49 +1,50 @@
 module.exports = (sequelize, DataTypes) => {
   const Course = sequelize.define(
-    'Course',
+    "Course",
     {
       title: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: { msg: 'Title is required.' },
-        },
+          notEmpty: { msg: "Title is required." }
+        }
       },
       description: {
         type: DataTypes.TEXT,
         allowNull: false,
         validate: {
-          notEmpty: { msg: 'Description is required.' },
-        },
+          notEmpty: { msg: "Description is required." }
+        }
       },
       estimatedTime: {
         type: DataTypes.STRING,
         allowNull: true,
         validate: {
           //
-        },
+        }
       },
       materialsNeeded: {
         type: DataTypes.STRING,
         allowNull: true,
         validate: {
           //
-        },
-      },
+        }
+      }
     },
     {
       // Options
-      sequelize,
-    },
+      sequelize
+    }
   );
 
-  Course.associate = (models) => {
+  Course.associate = models => {
     Course.belongsTo(models.User, {
-      // Will save to database ok without this,
-      // but needed for retrieval of key
-      foreignKey: 'userId',
       // If client doesn't supply a userId, API will respond with
       // "SQLITE_CONSTRAINT: FOREIGN KEY constraint failed"
+
+      // Will save to database ok without this,
+      // but needed for retrieval of key
+      foreignKey: "userId"
     });
   };
 

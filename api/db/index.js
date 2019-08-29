9f7db214d6c16d71f4ddb0aa408fa074b5fd6e6e
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-08-29 08:11:32 -0400

    Prettier changed ' to "

--- api/db/index.js
+++ api/db/index.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Imports Sequelize
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 // Creates instance of Sequelize
 const sequelize = new Sequelize({
   // Options
-  dialect: 'sqlite',
-  storage: 'fsjstd-restapi.db',
+  dialect: "sqlite",
+  storage: "fsjstd-restapi.db"
 });
 
 // Test connection
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connection successful!');
+    console.log("Connection successful!");
   } catch (error) {
-    console.log('Connection failed.');
+    console.log("Connection failed.");
   }
 })();
 
@@ -25,12 +25,12 @@ const models = {};
 
 // Boilerplate code adapted from express generator AND TreeHouse example 'data relationships'
 // Import models
-fs.readdirSync(path.join(__dirname, 'models')).forEach((file) => {
-  const model = sequelize.import(path.join(__dirname, 'models', file));
+fs.readdirSync(path.join(__dirname, "models")).forEach(file => {
+  const model = sequelize.import(path.join(__dirname, "models", file));
   models[model.name] = model;
 });
 // Create associations
-Object.keys(models).forEach((modelName) => {
+Object.keys(models).forEach(modelName => {
   if (models[modelName].associate) {
     models[modelName].associate(models);
   }
@@ -39,5 +39,5 @@ Object.keys(models).forEach((modelName) => {
 module.exports = {
   sequelize,
   Sequelize,
-  models,
+  models
 };

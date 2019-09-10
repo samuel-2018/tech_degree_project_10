b18eaf61a3e92afc9a6a330534443e751f179243
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-09-10 13:33:06 -0400

    added API production URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,5 @@
-export default { apiBaseUrl: 'http://localhost:5000/api' };
+// For local testing
+export default { apiBaseUrl: "http://localhost:5000/api" };
+
+// For production
+// export default { apiBaseUrl: "http://school-api.sam.stickle.us/api" };
